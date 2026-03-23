85b994446a6455588a096c1fa1565af4ecd19dce
Connie <conniedpaula@gmail.com>
2026-03-23 15:55:00 -0300

    add logic for multi-page pdf

--- src/application/export/constants.ts
+++ src/application/export/constants.ts
@@ -2,7 +2,8 @@
  * PDF export sizes in cm.
  */
 export const pdfSize = {
-  A0: { height: 118.9, width: 84.1 },
+  a0: { height: 118.9, width: 84.1 },
+  a4: { height: 29.7, width: 21.0 },
 };
 
 export type PdfSize = keyof typeof pdfSize;
